50d9a425b528660b40cbcd0dfba74ccdafda69b8
Brandon Keown <brandon.keown@taillight.com>
2017-06-30 10:23:26 -0500

    Adjust test to remove assertion for Promise subclass.

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ import PCancelable from '.';
 const fixture = Symbol('fixture');
 
 test('new PCancelable()', async t => {
-	t.plan(5);
+	t.plan(4);
 
 	const p = new PCancelable((onCancel, resolve) => {
 		onCancel(() => {
@@ -16,8 +16,6 @@ test('new PCancelable()', async t => {
 			resolve(fixture);
 		}, 50);
 	});
-
-	t.true(p instanceof Promise);
 
 	t.false(p.canceled);
 
